e0ecbe2d7419e7a74f163d56537c49717ac1fd7a
Craft <38158429+Craft485@users.noreply.github.com>
2021-07-10 13:08:43 -0400

    add(cmd): add help command

---
+++ assets/Help.ts
@@ -0,0 +1,45 @@
+import { Message, MessageEmbed } from 'discord.js'
+const config = require('../config.json')
+
+module.exports = {
+    name: 'help',
+    description: 'Shows a list of available commands',
+    aliases: ['h'],
+    usage: '[command name]',
+    execute: (msg: Message, args: Array<string>) => {
+        // @ts-ignore: property 'commands' doesn't exist on type Client
+        const { commands } = msg.client
+
+        let data = []
+
+        // Show the full list
+        if (!args.length) {
+            const commandList: Array<string> = commands.map(command => `${command.name} | ${command.description}`)
+            commandList.forEach(commandInfo => { data.push(commandInfo) })
+            const response = new MessageEmbed()
+                .setColor(config.colors.green)
+                .setTitle('Here\'s a list of my available commands')
+                .addField('=====', data.join('\n'))
+                .setFooter(`Use ${config.prefix}[command name] for more information on a specific command`)
+            return msg.channel.send(response)
+        }
+
+        // Show info on a specific command
+        const name = args[0].toLowerCase()
+        const command = commands.get(name) || commands.find(cmd => cmd.aliases && cmd.aliases.includes(name))
+
+        if (!command) return msg.reply(`${name} is an invalid command name, see ${config.prefix}help for a list of all possible commands`)
+
+        data.push(`**Name:** ${command.name}`)
+
+        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+		if (command.description) data.push(`**Description:** ${command.description}`);
+		if (command.usage) data.push(`**Usage:** ${config.prefix}${command.name} ${command.usage}`)
+
+        const response = new MessageEmbed()
+            .setColor(config.colors.green)
+            .addField('=====', data.join('\n'))
+
+        return msg.channel.send(response)
+    }
+}
